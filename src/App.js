7aefed74edec0abf0d4e313acb340566a7c7b3b5
Pawel Sioda <pawel.sioda@gmail.com>
2022-01-01 20:35:43 +0100

    delete comments from App

--- src/App.js
+++ src/App.js
@@ -4,18 +4,9 @@ import UsersContainer from "./components/UsersContainer";
 import "./styles.css";
 
 export default class App extends Component {
-  // state = {
-  //   show: false
-  // };
-  // handleToggle = () => {
-  //   this.setState({ show: !this.state.show });
-  // };
   render() {
-    // const { show } = this.state;
     return (
       <div className="App">
-        {/*<button onClick={this.handleToggle}>toggle</button>*/}
-        {/*{show && <UsersContainer />}*/}
         <UsersContainer />
       </div>
     );
